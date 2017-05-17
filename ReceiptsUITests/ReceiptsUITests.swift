e28fd9af0876d813d736cfd1866b5479ef1dc6d7
Mike Zornek <mike@clickablebliss.com>
2017-05-17 00:24:21 -0400

    Playing around with snapshot tool.

--- ReceiptsUITests/ReceiptsUITests.swift
+++ ReceiptsUITests/ReceiptsUITests.swift
@@ -1,26 +1,13 @@
-//
-//  ReceiptsUITests.swift
-//  ReceiptsUITests
-//
-//  Created by Michael Zornek on 4/25/17.
-//  Copyright © 2017 Zorn Labs. All rights reserved.
-//
-
 import XCTest
 
 class ReceiptsUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
     }
     
     override func tearDown() {
@@ -29,8 +16,25 @@ class ReceiptsUITests: XCTestCase {
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        
+        app.navigationBars["Receipts"].buttons["Refresh"].tap()
+        
+        snapshot("01ReceiptLists")
+        
+        let cells = XCUIApplication().tables.cells
+        let firstCell = cells.element(boundBy: 0)
+        let staticTextOfFirstCell = firstCell.staticTexts.element(boundBy: 0)
+        staticTextOfFirstCell.tap()
+        
+        snapshot("02ReceiptDetails")
+
+        let detailNavigationBar = app.navigationBars["Detail"]
+        detailNavigationBar.buttons["Edit"].tap()
+        
+        snapshot("02ReceiptEdit")
+        
     }
     
 }
---
+++ Snapfile
@@ -0,0 +1,27 @@
+devices([
+  "iPhone 6",
+  "iPhone 6 Plus",
+  "iPhone 5"
+])
+
+languages([
+  "en-US",
+  "de-DE"
+])
+
+# The name of the scheme which contains the UI Tests
+scheme "ReceiptsScreenshots"
+
+# Where should the resulting screenshots be stored?
+output_directory "./screenshots"
+
+clear_previous_screenshots true
+
+# Choose which project/workspace to use
+project "./Receipts.xcodeproj"
+
+# Arguments to pass to the app on launch. See https://github.com/fastlane/fastlane/tree/master/snapshot#launch-arguments
+# launch_arguments(["-favColor red"])
+
+# For more information about all available options run
+# fastlane snapshot --help
